1229241373c938b1b028fabd4c6f482b503a3b35
Gopi-22 <121621660+Gopi-22@users.noreply.github.com>
2023-03-29 18:22:49 +0000

    Add files via upload

---
+++ pencil_sketch.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+img=cv2.imread("/home/gopi/image_to_pencil_sketch/car.jpg")
+
+resize = cv2.resize(img,(0,0),fx=0.4,fy=0.4)
+
+gray_img=cv2.cvtColor(resize,cv2.COLOR_BGR2GRAY)
+invert=cv2.bitwise_not(gray_img)
+blur=cv2.GaussianBlur(invert,(21,21),0)
+invertedblur=cv2.bitwise_not(blur)
+sketch=cv2.divide(gray_img,invertedblur,scale=200)
+
+
+
+cv2.imshow("Image",sketch)
+cv2.waitKey(6000)
+cv2.destroyAllWindows()
+
+status=cv2.imwrite("/home/gopi/image_to_pencil_sketch/car1.jpeg",sketch)
+print("Image written to file-system",status)
